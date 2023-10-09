82cb7d38d0e4f273776363144cbe95530ba76122
Rifat Ahmed <fahim363501@gmail.com>
2023-10-09 10:16:43 +0600

    wrapper added

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,19 +1,22 @@
+import Card from "../ui/Card";
 import classes from "./Meetupitem.module.css";
 
 function MeetupItem(props) {
   return (
     <li className={classes.item}>
-      <div className={classes.image}>
-        <img src={props.image} alt="" />
-      </div>
-      <div className={classes.content}>
-        <h3>TITLE</h3>
-        <address>{props.address}</address>
-        <p>{props.description}</p>
-      </div>
-      <div className={classes.actions}>
-        <button>TO Favourites</button>
-      </div>
+      <Card>
+        <div className={classes.image}>
+          <img src={props.image} alt="" />
+        </div>
+        <div className={classes.content}>
+          <h3>TITLE</h3>
+          <address>{props.address}</address>
+          <p>{props.description}</p>
+        </div>
+        <div className={classes.actions}>
+          <button>TO Favourites</button>
+        </div>
+      </Card>
     </li>
   );
 }
