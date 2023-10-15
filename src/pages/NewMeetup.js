ac8ec98a65a70555d5ab5aa3b8ce27c58f800b0a
Rifat Ahmed <fahim363501@gmail.com>
2023-10-15 22:24:05 +0600

    firebase post method added

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,28 @@
+import { useHistory } from "react-router-dom";
 import NewMeetupform from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage(params) {
+  const history = useHistory();
+
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-getting-started-dad17-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      history.replace("/");
+    });
+  }
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupform />
+      <NewMeetupform onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
